063f1702229c2a26c445f2dce86cce865ee25798
zach <102389340+zw0ng@users.noreply.github.com>
2024-01-09 18:07:28 -0800

    added table component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5aa78b540c8de48d"; }
+/******/ 		__webpack_require__.h = function() { return "99db0a233894c09a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
