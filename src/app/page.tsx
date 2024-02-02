2c70cb12e946cb90f2fff46ddbe4dab43d4410b6
zach <102389340+zw0ng@users.noreply.github.com>
2024-02-02 01:02:08 -0800

    fixed alignment issue on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Spacer from '@/components/Spacer'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center w-full h-[100vh]">
+    <main className="flex flex-col items-center justify-center w-full h-[80vh]">
       <ProfileForm />
     </main>
   )
